aaad4866de7517cb9701ae7e3db7871bb053d7ce
J-E Castagnede <j.e.castagnede@gmail.com>
2017-02-01 16:09:23 +0100

    fix logos

--- .salt/files/overlay/geotrek/settings/custom.py
+++ .salt/files/overlay/geotrek/settings/custom.py
@@ -4,15 +4,17 @@ from .prod import *
 # Necessary block of config when maps are not from Geotrek Tilecache :
 LEAFLET_CONFIG['SRID'] = 3857
 LEAFLET_CONFIG['TILES'] = [
-    (gettext_noop('Scan Express'), 'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Scan'),
+     'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Scan Express'), 'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
     (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('OSM'), 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', '(c) OpenStreetMap Contributors'),
-    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Cadastre'),
+     'http://gpp3-wxs.ign.fr/9s64n7xq9dz1fhxe56113pan/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
+    (gettext_noop('OSM'), 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', '(c) OpenStreetMap Contributors'),
 ]
 
 SPLIT_TREKS_CATEGORIES_BY_PRACTICE = True
